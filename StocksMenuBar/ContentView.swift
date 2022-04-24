c1d02cee7b35f2e83bd46566f6723364c9600248
Mohammad Azam <azamsharp@gmail.com>
2022-04-24 14:35:50 -0500

    Initial Commit

---
+++ StocksMenuBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksMenuBar
+//
+//  Created by Mohammad Azam on 4/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksMenuBar/StocksMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksMenuBarApp.swift
+//  StocksMenuBar
+//
+//  Created by Mohammad Azam on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
